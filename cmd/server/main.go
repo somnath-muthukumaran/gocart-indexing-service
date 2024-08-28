6faf42098c27b1a196018ee5d30e0c859dcd2a40
somnath-muthukumaran <somnath@edvoy.com>
2024-08-28 18:20:15 +0530

    feat:config setup changes

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/niklaus-mikael/gocart/indexing-service/internal/config"
+	"github.com/niklaus-mikael/gocart/indexing-service/internal/esearch"
+	"github.com/niklaus-mikael/gocart/indexing-service/internal/rmq"
+	"github.com/niklaus-mikael/gocart/indexing-service/internal/services"
+)
+
+func main() {
+	fmt.Println("Checking")
+	config.LoadConfig()
+	esearch.Init()
+	_, err := rmq.Connect()
+	if err != nil {
+		log.Fatal("Failed to connect to RabbitMQ:", err)
+	}
+	conn, err := rmq.GetConnection()
+	if err != nil {
+		log.Fatal("Failed to retrieve RabbitMQ connection:", err)
+	}
+	defer conn.Close()
+	serviceManager := services.NewServiceManager()
+	serviceManager.StartAll()
+}
